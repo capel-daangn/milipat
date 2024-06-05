f26eb92bcbef17fa51220fc58d07bda9aea3a784
alex <basecamp@alexs-MacBook-Pro.local>
2024-06-06 01:29:07 +0900

    .

--- app/fe/next.config.js
+++ app/fe/next.config.js
@@ -30,6 +30,18 @@ const withPWA = require("next-pwa")({
 const withVideos = require("next-videos");
 const nextConfig = {
   reactStrictMode: false,
+  swcMinify: true,
+  async rewrite() {
+    return [
+      {
+        // localhost:9000의 api 요청할 때 /api/~~ 로작성하면 'http://localhost:9000/~~' 로 요청한 것과 동일하게 적용이 된다.
+        source: "/",
+        // cors로 문제가 되었던 url 입력
+        destination:
+          "http://ec2-13-124-175-105.ap-northeast-2.compute.amazonaws.com:8081",
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.module.rules.push({
