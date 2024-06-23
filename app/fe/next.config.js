671c6991ba61fdf962e9889af3f9cb2f36d191f4
JIUK <alex.jiuk.kim@gmail.com>
2024-06-23 16:00:25 +0900

    ✨ Add LLM Choice

--- app/fe/next.config.js
+++ app/fe/next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  minimumCacheTTL: 60,
   // async rewrite() {
   //   return [
   //     {
--- app/fe/src/components/chart/worldmap-chart.tsx
+++ app/fe/src/components/chart/worldmap-chart.tsx
@@ -1,18 +1,18 @@
 import WorldMap from "react-svg-worldmap";
 
-export default function WorldmapChart(props: { size: any }) {
-  const data = [
-    { country: "cn", value: 4778 }, // china
-    { country: "in", value: 1204 }, // india
-    { country: "us", value: 5986 }, // united states
-    { country: "id", value: 824 }, // indonesia
-    { country: "pk", value: 836 }, // pakistan
-    { country: "br", value: 591 }, // brazil
-    { country: "ng", value: 114 }, // nigeria
-    { country: "bd", value: 905 }, // bangladesh
-    { country: "ru", value: 241 }, // russia
-    { country: "mx", value: 112 }, // mexico
-  ];
+export default function WorldmapChart(props: { size: any; data: any }) {
+  // const data = [
+  //   { country: "cn", value: 4778 }, // china
+  //   { country: "in", value: 1204 }, // india
+  //   { country: "us", value: 5986 }, // united states
+  //   { country: "id", value: 824 }, // indonesia
+  //   { country: "pk", value: 836 }, // pakistan
+  //   { country: "br", value: 591 }, // brazil
+  //   { country: "ng", value: 114 }, // nigeria
+  //   { country: "bd", value: 905 }, // bangladesh
+  //   { country: "ru", value: 241 }, // russia
+  //   { country: "mx", value: 112 }, // mexico
+  // ];
 
   return (
     <WorldMap
@@ -20,7 +20,7 @@ export default function WorldmapChart(props: { size: any }) {
       // title="Top 10 Populous Countries"
       // value-suffix="people"
       size={props.size}
-      data={data}
+      data={props.data}
     />
   );
 }
