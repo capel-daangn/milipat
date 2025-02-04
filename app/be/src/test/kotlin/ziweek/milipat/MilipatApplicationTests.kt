5aff5c610d15ff423cfbb62ae467e64f6947ca29
ziweek <alex.jiuk.kim@gmail.com>
2025-02-05 03:30:47 +0900

    restructure

---
+++ app/be/src/test/kotlin/ziweek/milipat/MilipatApplicationTests.kt
@@ -0,0 +1,13 @@
+package ziweek.milipat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MilipatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
